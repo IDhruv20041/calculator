f88f822da561bcdecf522305cf14eae9525dbfca
Arpit Patidar <arpit.ptdr@hotmail.com>
2023-05-17 13:47:49 +0530

    feat: added arithmatic functions

--- main.cpp
+++ main.cpp
@@ -1,1 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include "arithematic.cpp"
+
+using namespace std;
+
+int main() {
+    char mode;
+    int a, b, result;
+
+    cout << "Enter mode";
+    cin >> mode;
+
+    cout << "Enter first number";
+    cin >> a;
+
+    cout << "Enter second number";
+    cin >> b;
+
+    switch(mode) {
+        case '+':
+            result = add(a, b);
+            break;
+
+        case '-':
+            result = subtract(a, b);
+            break;
+
+        case '*':
+            result = multiply(a, b);
+            break;
+
+        case '/':
+            result = divide(a, b);
+            break;
+
+        default:
+            result = 0;
+            cout << "You have entered invalid mode";
+            break;
+    }
+
+    if (result != 0) cout << result;
+    return 0;
+}
